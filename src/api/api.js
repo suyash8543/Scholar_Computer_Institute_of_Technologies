00b0031167d1258013c54f9f8bfcc84a512f4b85
Suyash Tripathi <stripathi8543@mail.com>
2026-07-04 22:18:13 +0530

    Initial commit

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://localhost:5001/api"
-    // Change this to your backend URL
-});
-console.log("API Base URL:", api.defaults.baseURL);
-export default api;
