95f1b126700449599bd01e373bcdc3e48c1c4cd9
Suyash Tripathi <stripathi8543@mail.com>
2026-07-05 19:20:59 +0530

    Backend Connected

--- scholar-institute_Frontend/src/api/api.js
+++ scholar-institute_Frontend/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://scholar-computer-institute-of-oox1.onrender.com"
-    // Change this to your backend URL
+    baseURL: "https://scholar-computer-institute-of-oox1.onrender.com/api"
 });
+
 console.log("API Base URL:", api.defaults.baseURL);
+
 export default api;
